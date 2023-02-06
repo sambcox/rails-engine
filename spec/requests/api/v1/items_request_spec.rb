6ae2ab52d0b8f22b48252a5d236b00973c61ad5b
Samuel Cox <samc1253@gmail.com>
2023-02-06 16:17:09 -0700

    Add item show test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -31,4 +31,30 @@ describe "Items Request" do
       expect(found_item.merchant_id).to eq(item[:attributes][:merchant_id])
     end
   end
+
+  it "sends a single item" do
+    create_list(:merchant, 3)
+    create_list(:item, 10)
+
+    item_raw = Item.first
+
+    get "/api/v1/items/#{item_raw.id}"
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    item = data[:data]
+
+    expect(item).to have_key(:id)
+    expect(item_raw.id).to eq(item[:id].to_i)
+    expect(item[:attributes]).to have_key(:name)
+    expect(item_raw.name).to eq(item[:attributes][:name])
+    expect(item[:attributes]).to have_key(:description)
+    expect(item_raw.description).to eq(item[:attributes][:description])
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item_raw.unit_price).to eq(item[:attributes][:unit_price])
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item_raw.merchant_id).to eq(item[:attributes][:merchant_id])
+  end
 end
