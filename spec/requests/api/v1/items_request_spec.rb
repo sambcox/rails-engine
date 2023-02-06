f394619549e31a46212156d451669f2a90a24538
Samuel Cox <samc1253@gmail.com>
2023-02-06 16:08:47 -0700

    Create tests for item requests

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "Items Request" do
+  it "sends a list of items" do
+    create_list(:merchant, 3)
+    create_list(:item, 10)
+
+    items_raw = Item.all
+
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    items = data[:data]
+
+    expect(items.count).to eq(10)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      found_item = Item.find(item[:id])
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(found_item.name).to eq(item[:attributes][:name])
+      expect(item[:attributes]).to have_key(:description)
+      expect(found_item.description).to eq(item[:attributes][:description])
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(found_item.unit_price).to eq(item[:attributes][:unit_price])
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(found_item.merchant_id).to eq(item[:attributes][:merchant_id])
+    end
+  end
+end
