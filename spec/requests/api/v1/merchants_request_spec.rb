1b3f2e59fd34b8d63869234ab0f52b27514c8b6c
Samuel Cox <samc1253@gmail.com>
2023-02-06 15:01:17 -0700

    Add test for singular merchant return

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -22,4 +22,24 @@ describe "Merchants Request" do
       expect(Merchant.exists?(name: merchant[:attributes][:name])).to eq true
     end
   end
+
+  it "sends one merchant" do
+    create_list(:merchant, 3)
+
+    merchant_raw = Merchant.first
+
+    get "/api/v1/merchants/#{merchant_raw.id}"
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    merchant = data[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(Merchant.exists?(id: merchant[:id])).to eq true
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(Merchant.exists?(name: merchant[:attributes][:name])).to eq true
+  end
 end
