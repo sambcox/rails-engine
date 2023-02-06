8c78c503a14068f44e7996ee58025c0419866d8e
Samuel Cox <samc1253@gmail.com>
2023-02-06 16:31:11 -0700

    Add test for creating an item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -57,4 +57,24 @@ describe "Items Request" do
     expect(item[:attributes]).to have_key(:merchant_id)
     expect(item_raw.merchant_id).to eq(item[:attributes][:merchant_id])
   end
+
+  it "can create a new item" do
+    create_list(:merchant, 3)
+    item_params = ({
+                    name: Faker::Commerce.product_name,
+                    description: Faker::TvShows::Community.quotes,
+                    unit_price: Faker::Number.within(range: 1.0..1000.0),
+                    merchant_id: Merchant.all.shuffle.shuffle.first.id
+                  })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(book_params[:name])
+    expect(created_item.description).to eq(book_params[:description])
+    expect(created_item.unit_price).to eq(book_params[:unit_price])
+    expect(created_item.merchant_id).to eq(book_params[:merchant_id])
+  end
 end
