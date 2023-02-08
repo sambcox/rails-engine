e7b747b77d5721d41ac034bbc97e39183baa01a3
Samuel Cox <samc1253@gmail.com>
2023-02-07 17:18:26 -0700

    Add test for searching by description

--- spec/requests/api/v1/items/find/item_find_all_get_request_spec.rb
+++ spec/requests/api/v1/items/find/item_find_all_get_request_spec.rb
@@ -14,6 +14,8 @@ describe "Find Items Get Request" do
 
     items = data[:data]
 
+    expect(items.count).to eq(5)
+
     item_ids = findable_items.map(&:id)
 
     items.each do |item|
@@ -38,6 +40,8 @@ describe "Find Items Get Request" do
 
     items = data[:data]
 
+    expect(items.count).to eq(5)
+
     item_ids = findable_items.map(&:id)
 
     items.each do |item|
@@ -46,6 +50,33 @@ describe "Find Items Get Request" do
 
       expect(item[:attributes]).to have_key(:name)
       expect(item[:attributes][:name]).to eq('Jordan 191')
+    end
+  end
+
+
+  it "returns all items given a description" do
+    create_list(:merchant, 3)
+    findable_items = create_list(:item, 5, description: 'Jordan 191')
+    lost_items = create_list(:item, 5, description: "Ferrari 641")
+
+    get "/api/v1/items/find_all?name=#{'Jordan 191'}"
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    items = data[:data]
+
+    expect(items.count).to eq(5)
+
+    item_ids = findable_items.map(&:id)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item_ids).to include(item[:id].to_i)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:description]).to eq('Jordan 191')
     end
   end
 end
